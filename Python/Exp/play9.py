14fe077c23b619568dcba057cad4e1603d138c93
blockpit <blockpit@gmail.com>
2018-06-24 18:39:20 +1000

    This is the biggest commit of the decade!

---
+++ Python/Exp/play9.py
@@ -0,0 +1,138 @@
+# Standard library experimentation
+
+print(abs(-2))
+
+print( all( [True,True,True,True,True] ) )
+
+print( all( [True,True,True,False] ) )
+
+print( any( [False,False,False,False] ) )
+
+print( any( [False,False,False,True] ) )
+
+print( ascii('nhuoehoeĤģĴ') )
+
+print( bin(93827) )
+
+print( bin(100) )
+
+print( bin(-7932) )
+
+print( bool() )
+
+print( bool(420) )
+
+print( bool(3328904032) )
+
+print( bytearray (9) )
+
+nthe = 2
+
+print( callable( nthe ) ) 
+
+def calls(yo):
+    return yo
+
+print( callable( calls ) )
+
+# print( callable( yo ) )
+
+print( chr( 8364 ) )
+
+print( chr( 1114111 ) )
+
+# compile(a = 1)
+
+print ( complex(1,1) )
+
+# test1 = dict(44)
+# print(test1)
+
+print( dir() )
+
+print( dir('__builtins__'))
+
+print( divmod(10,20) )
+
+coders = ['Mark','Guido','Rasmus']
+
+print( list( enumerate( coders, 10 ) ) )
+
+mn = 19
+
+print( eval('56 * (mn + 8) * 2') )
+
+mn2 = eval('mn*23')
+
+print(mn2)
+
+print( float(3))
+
+print( float(3.230498203499357859378439878))
+
+print( float( '4' ))
+
+print( format( 'sup' ))
+
+# print( getattr('4'))
+
+print( globals())
+
+list1 = ['London','Mayfair','Piccadilly']
+
+print( hasattr(list1, 'London'))  #????
+
+print( hash('hello'))
+
+print( hash('sweet'))
+
+# print( help(str))
+
+print( hex(100))
+
+print( hex(126))
+
+print( hex(20))
+
+print( id('help'))
+
+# firstin = input('How are you?')
+
+# print(firstin)
+
+print( int('425890abcdef',16))
+
+print( int('abcbabcab',13))
+
+# print( int('abcbabcab',12))
+
+print( len('Hello') )
+
+print( len( (1,2) ) )
+
+print( len( [1,2,3,4] ) )
+
+print( len( {'hi' : 2, 'ih' : 3} ) )
+
+print( locals() )
+
+print( map( 2 , [1,2] ) )
+
+print( max( [100,1,101] ) )
+
+# memview = memoryview(h'hi')
+
+print( min( [423,243,77686] ) )
+
+print( min( 'abc' ) )
+
+print( min( 'hello' ) )
+
+# print( next( [1,2,3] ) ) 
+
+print( oct( 48 ) )
+
+print( oct( 24 ) )
+
+# open()
+
